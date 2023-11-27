6762ba9b947a0e19eec59ed828bb92c6e61f672b
Fahridanaa <fahridanaaa@gmail.com>
2023-11-27 11:51:36 +0700

    menambahkan warna icon di sidebar ketika dihover

--- components/sidebar.php
+++ components/sidebar.php
@@ -1,63 +1,61 @@
 <div class="bg-primary-color h-screen">
   <div class="flex w-32 flex-col items-center justify-around h-screen">
-    <button class="flex items-center justify-center rounded-md">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-        class="w-14 h-14 fill-white hover:fill-fourth-color">
-        <path d="M16.5 7.5h-9v9h9v-9z" />
-        <path fill-rule="evenodd"
-          d="M8.25 2.25A.75.75 0 019 3v.75h2.25V3a.75.75 0 011.5 0v.75H15V3a.75.75 0 011.5 0v.75h.75a3 3 0 013 3v.75H21A.75.75 0 0121 9h-.75v2.25H21a.75.75 0 010 1.5h-.75V15H21a.75.75 0 010 1.5h-.75v.75a3 3 0 01-3 3h-.75V21a.75.75 0 01-1.5 0v-.75h-2.25V21a.75.75 0 01-1.5 0v-.75H9V21a.75.75 0 01-1.5 0v-.75h-.75a3 3 0 01-3-3v-.75H3A.75.75 0 013 15h.75v-2.25H3a.75.75 0 010-1.5h.75V9H3a.75.75 0 010-1.5h.75v-.75a3 3 0 013-3h.75V3a.75.75 0 01.75-.75zM6 6.75A.75.75 0 016.75 6h10.5a.75.75 0 01.75.75v10.5a.75.75 0 01-.75.75H6.75a.75.75 0 01-.75-.75V6.75z"
-          clip-rule="evenodd" />
-      </svg>
-    </button>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+      class="w-14 h-14 fill-white">
+      <path d="M16.5 7.5h-9v9h9v-9z" />
+      <path fill-rule="evenodd"
+        d="M8.25 2.25A.75.75 0 019 3v.75h2.25V3a.75.75 0 011.5 0v.75H15V3a.75.75 0 011.5 0v.75h.75a3 3 0 013 3v.75H21A.75.75 0 0121 9h-.75v2.25H21a.75.75 0 010 1.5h-.75V15H21a.75.75 0 010 1.5h-.75v.75a3 3 0 01-3 3h-.75V21a.75.75 0 01-1.5 0v-.75h-2.25V21a.75.75 0 01-1.5 0v-.75H9V21a.75.75 0 01-1.5 0v-.75h-.75a3 3 0 01-3-3v-.75H3A.75.75 0 013 15h.75v-2.25H3a.75.75 0 010-1.5h.75V9H3a.75.75 0 010-1.5h.75v-.75a3 3 0 013-3h.75V3a.75.75 0 01.75-.75zM6 6.75A.75.75 0 016.75 6h10.5a.75.75 0 01.75.75v10.5a.75.75 0 01-.75.75H6.75a.75.75 0 01-.75-.75V6.75z"
+        clip-rule="evenodd" />
+    </svg>
 
     <div class="flex flex-col justify-between rounded-md">
       <ul>
         <li class="p-5 flex items-center justify-center">
-          <button class="flex items-center justify-center rounded-md flex-col gap-4">
+          <button class="flex items-center justify-center rounded-md flex-col gap-4 group">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-              class="w-6 h-6 fill-white hover:fill-fourth-color">
+              class="w-6 h-6 fill-white group-hover:fill-fourth-color">
               <path
                 d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
               <path
                 d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
             </svg>
-            <span class="text-neutral-color">Home</span>
+            <span class="text-neutral-color group-hover:text-fourth-color">Home</span>
           </button>
         </li>
         <li class="p-5 flex items-center justify-center">
-          <button class="flex items-center justify-center rounded-md flex-col gap-4">
+          <button class="flex items-center justify-center rounded-md flex-col gap-4 group">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-              class="w-6 h-6 fill-white hover:fill-fourth-color">
+              class="w-6 h-6 fill-white group-hover:fill-fourth-color">
               <path
                 d="M11.25 4.533A9.707 9.707 0 006 3a9.735 9.735 0 00-3.25.555.75.75 0 00-.5.707v14.25a.75.75 0 001 .707A8.237 8.237 0 016 18.75c1.995 0 3.823.707 5.25 1.886V4.533zM12.75 20.636A8.214 8.214 0 0118 18.75c.966 0 1.89.166 2.75.47a.75.75 0 001-.708V4.262a.75.75 0 00-.5-.707A9.735 9.735 0 0018 3a9.707 9.707 0 00-5.25 1.533v16.103z" />
             </svg>
-            <span class="text-neutral-color">Pinjam</span>
+            <span class="text-neutral-color group-hover:text-fourth-color">Pinjam</span>
           </button>
 
         </li>
         <li class="p-5 flex items-center justify-center">
-          <button class="flex items-center justify-center rounded-md flex-col gap-4">
+          <button class="flex items-center justify-center rounded-md flex-col gap-4 group">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-              class="w-6 h-6 fill-white hover:fill-fourth-color">
+              class="w-6 h-6 fill-white group-hover:fill-fourth-color">
               <path fill-rule="evenodd"
                 d="M6.912 3a3 3 0 00-2.868 2.118l-2.411 7.838a3 3 0 00-.133.882V18a3 3 0 003 3h15a3 3 0 003-3v-4.162c0-.299-.045-.596-.133-.882l-2.412-7.838A3 3 0 0017.088 3H6.912zm13.823 9.75l-2.213-7.191A1.5 1.5 0 0017.088 4.5H6.912a1.5 1.5 0 00-1.434 1.059L3.265 12.75H6.11a3 3 0 012.684 1.658l.256.513a1.5 1.5 0 001.342.829h3.218a1.5 1.5 0 001.342-.83l.256-.512a3 3 0 012.684-1.658h2.844z"
                 clip-rule="evenodd" />
             </svg>
-            <span class="text-neutral-color">Inbox</span>
+            <span class="text-neutral-color group-hover:text-fourth-color">Inbox</span>
           </button>
 
         </li>
         <li class="p-5 flex items-center justify-center">
-          <button class="flex items-center justify-center rounded-md flex-col gap-4">
+          <button class="flex items-center justify-center rounded-md flex-col gap-4 group">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-              class="w-6 h-6 fill-white hover:fill-fourth-color">
+              class="w-6 h-6 fill-white group-hover:fill-fourth-color">
               <path fill-rule="evenodd"
                 d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0016.5 9h-1.875a1.875 1.875 0 01-1.875-1.875V5.25A3.75 3.75 0 009 1.5H5.625zM7.5 15a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 017.5 15zm.75 2.25a.75.75 0 000 1.5H12a.75.75 0 000-1.5H8.25z"
                 clip-rule="evenodd" />
               <path
                 d="M12.971 1.816A5.23 5.23 0 0114.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 013.434 1.279 9.768 9.768 0 00-6.963-6.963z" />
             </svg>
-            <span class="text-neutral-color">History</span>
+            <span class="text-neutral-color group-hover:text-fourth-color">History</span>
           </button>
 
         </li>
