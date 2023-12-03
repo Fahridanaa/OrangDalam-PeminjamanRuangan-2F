ca3c819de1f5ddd5f2740545b7a817a6a0c60fa9
Muhammad Fakhruddin Arif <muhfakhruddin30@gmail.com>
2023-12-03 11:48:33 +0700

    Update Class Jurusan

---
+++ app/Models/Jurusan.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace OrangDalam\PeminjamanRuangan\Models;
+
+class Jurusan
+{
+    private $table = 'jurusan';
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new \Database();
+    }
+
+    public function getJurusan() {
+        $this->db->query('SELECT * FROM' . $this->table);
+        return $this->db->resultSet();
+    }
+}
+
+$jurusan = new Jurusan();
