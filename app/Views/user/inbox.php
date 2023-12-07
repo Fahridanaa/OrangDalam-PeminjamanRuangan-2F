9dc99c638aba27404392e1e2bbe1058da337a2ca
Fahridanaa <fahridanaaa@gmail.com>
2023-12-07 11:28:16 +0700

    add inbox text

--- app/Views/user/inbox.php
+++ app/Views/user/inbox.php
@@ -7,12 +7,14 @@
 <body>
 <div id="Inbox" class="h-screen flex flex-row">
     <?php include 'sidebar.php'; ?>
-    <div id="Inbox-content" class="h-screen w-screen py-20">
-        <div id="header" class="px-8">
+    <div id="Inbox-content" class="h-screen w-screen py-20 px-8 flex flex-col gap-12">
+        <div id="header">
             <h1 class="text-4xl font-semibold mb-6">Inbox Pesan</h1>
             <hr class="border border-black">
         </div>
-
+        <div class="flex-auto flex justify-center">
+            <span class="text-xl font-medium">Belum ada Konfirmasi</span>
+        </div>
     </div>
 </div>
 </body>
