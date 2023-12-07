16db8cfa815fd4e2ecd32d512eab7cddcf8b15d1
Fahridanaa <fahridanaaa@gmail.com>
2023-12-07 21:57:05 +0700

    add cari ruangan button

--- app/Views/user/dashboard.php
+++ app/Views/user/dashboard.php
@@ -15,8 +15,8 @@
             <hr class="border border-black">
         </div>
         <div id="option" class="flex gap-8 px-8 py-5">
-            <div id="lantai">
-                <select name="lantai" id="lantai" class="py-3 px-16 rounded-lg border border-primary-color"
+            <div id="dashboard-lantai-option">
+                <select name="lantai" id="lantai" class="py-4 px-16 rounded-lg border border-primary-color"
                         title="Lantai">
                     <option value="5">Lantai 5</option>
                     <option value="6">Lantai 6</option>
@@ -24,10 +24,19 @@
                     <option value="8">Lantai 8</option>
                 </select>
             </div>
-            <div id="tanggal">
+            <div id="dashboard-tanggal-option">
                 <input type="date" name="tanggal" id="tanggal"
                        class="py-3 pl-8 pr-5 rounded-lg border border-primary-color">
             </div>
+            <button class="py-2 px-3 rounded-lg border bg-primary-color self-center items-center flex gap-2">
+                <span class="font-bold text-sm text-neutral-color">CARI RUANGAN</span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                     stroke="currentColor" class="w-4 h-4 text-neutral-color">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                          d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"/>
+                </svg>
+
+            </button>
         </div>
         <div id="denah"
              class="bg-[#E3E3E3] flex justify-between mx-auto gap-8 px-16 w-4/5 mt-8 rounded-3xl drop-shadow-xl shadow-md shadow-[#00000025] border-2 border-fourth-color">
