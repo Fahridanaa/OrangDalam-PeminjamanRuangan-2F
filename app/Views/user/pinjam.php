0f29dc8e68d002d47bd1a18ed8719d5f38962a81
Fahridanaa <fahridanaaa@gmail.com>
2023-12-13 21:35:15 +0700

    Flasher (#35)
    * add modals in admin and user
    
    * merge main
    
    * add flash message in login
    
    * main (#34)
    
    * new middleware (#33)
    
    * router detail (#32)
    
    ---------
    
    Co-authored-by: Abiyyu Fiqlal <115953049+Fikuraru72@users.noreply.github.com>
    
    * add flash message in profile page
    
    ---------
    
    Co-authored-by: Abiyyu Fiqlal <115953049+Fikuraru72@users.noreply.github.com>

--- app/Views/user/pinjam.php
+++ app/Views/user/pinjam.php
@@ -9,7 +9,9 @@
 <div id="Peminjaman" class="h-screen flex flex-row">
     <?php
     include 'sidebar.php';
+
     use OrangDalam\PeminjamanRuangan\Controllers\PeminjamanController;
+
     ?>
     <div id="Peminjaman-content" class="h-screen w-screen px-8 py-20 flex flex-col gap-12 ml-32">
         <div id="header">
@@ -45,23 +47,23 @@
                         $color = 'bg-select-color';
                         break;
                 }
-            ?>
+                ?>
                 <div class="flex flex-col border-2 border-[#7B7777] items-start p-5 rounded-xl gap-4 shadow-[0_4px_4px_0px_#00000025]">
                     <div class="flex w-full justify-between">
-                      <div class="flex gap-4">
-                         <span class="text-neutral-color px-3 py-1 <?= $color ?> rounded-xl"><?= $item['status'];?></span>
-                        // tambahi category
-                        <span class="acara text-neutral-color px-3 py-1 bg-locked-color rounded-xl">Acara</span> 
-                    </div>
+                        <div class="flex gap-4">
+                            <span class="text-neutral-color px-3 py-1 <?= $color ?> rounded-xl"><?= $item['status']; ?></span>
+                            // tambahi category
+                            <span class="acara text-neutral-color px-3 py-1 bg-locked-color rounded-xl">Acara</span>
+                        </div>
                         <?= $notif ?>
                     </div>
                     <div class="flex flex-col gap-1">
-                        <span class="font-bold text-3xl"><?= $item['kode_ruang'];?></span>
+                        <span class="font-bold text-3xl"><?= $item['kode_ruang']; ?></span>
                         <span class="font-normal text-xl">Digunakan Tanggal <?= $item['tanggalAcara'] ?></span>
                     </div>
                     <div class="self-end flex gap-5">
                         <?= $buttonSurat ?>
-                      // iki class e diganti antara matkul sama acara
+                        // iki class e diganti antara matkul sama acara
                         <button class="detail-matkul-button ont-bold text-sm px-3 py-2 bg-third-color rounded-3xl text-neutral-color hover:bg-primary-color">
                             Detail
                             Peminjaman
