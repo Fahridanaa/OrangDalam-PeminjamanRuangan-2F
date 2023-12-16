0a6a398b20cf4fea980456d0555c3cf71d332396
Fahridanaa <fahridanaaa@gmail.com>
2023-12-16 21:11:41 +0700

    melakukan refactoring kode

--- public/index.php
+++ public/index.php
@@ -2,48 +2,66 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use OrangDalam\PeminjamanRuangan\Core\Router;
-use OrangDalam\PeminjamanRuangan\Controllers\AuthController;
-
-use OrangDalam\PeminjamanRuangan\Controllers\User\DashboardController;
-use OrangDalam\PeminjamanRuangan\Controllers\User\PinjamController;
-use OrangDalam\PeminjamanRuangan\Controllers\User\MultiFormController;
-use OrangDalam\PeminjamanRuangan\Controllers\User\InboxController;
-use OrangDalam\PeminjamanRuangan\Controllers\User\DetailRuanganController;
-use OrangDalam\PeminjamanRuangan\Controllers\User\HistoryController;
-use OrangDalam\PeminjamanRuangan\Controllers\User\ProfileController;
-use OrangDalam\PeminjamanRuangan\Controllers\User\RequestController;
-use OrangDalam\PeminjamanRuangan\Controllers\User\PeminjamanController;
-
-use OrangDalam\PeminjamanRuangan\Controllers\Admin\AdminHistoryController;
-use OrangDalam\PeminjamanRuangan\Controllers\Admin\AdminInboxController;
-use OrangDalam\PeminjamanRuangan\Controllers\Admin\AdminKonfirmasiController;
+use OrangDalam\PeminjamanRuangan\Controllers\{
+    AuthController,
+    User\DashboardController,
+    User\PinjamController,
+    User\MultiFormController,
+    User\InboxController,
+    User\DetailRuanganController,
+    User\HistoryController,
+    User\ProfileController,
+    User\RequestController,
+    Admin\AdminHistoryController,
+    Admin\AdminInboxController,
+    Admin\AdminKonfirmasiController
+};
 
 session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-
-// core
-Router::add("GET", "/login", AuthController::class, "showLoginForm");
-Router::add("POST", "/login", AuthController::class, "processLogin");
-Router::add("GET", "/logout", AuthController::class, "logout");
-Router::add("GET", "/dashboard", AuthController::class, "showDashboard");
-
-
-// // user
-Router::add("POST", "/dashboard", DashboardController::class, "showDenah");
-Router::add("GET", "/pinjam", PinjamController::class, "showPinjamPage");
-Router::add("GET", "/pesan", InboxController::class, "showInboxPage");
-Router::add("GET", "/riwayat", HistoryController::class, "showHistoryPage");
-Router::add("GET", "/konfirmasi-ruangan", RequestController::class, "showRequestPage");
-Router::add("GET", "/profile", ProfileController::class, "showProfile");
-Router::add("GET", "/detail", DetailRuanganController::class, "showDetailRuangan");
-Router::add("POST", "/profile", AuthController::class, "changePass");
-Router::add("GET", "/pinjam/form", MultiFormController::class, "showForm");
-Router::add("POST", "/pinjam/form?step=2&category=acara", PeminjamanController::class, "insertAcara");
-
-// admin
-Router::add("GET", "/inbox", AdminInboxController::class, "showInbox");
-Router::add("GET", "/history", AdminHistoryController::class, "showHistory");
-Router::add("GET", "/konfirmasiPinjam", AdminKonfirmasiController::class, "showKonfirmasiPinjamPage");
+addCoreRoutes();
+addUserRoutes();
+addFormRoutes();
+addAdminRoutes();
 
 Router::run();
 
+function addCoreRoutes()
+{
+    // core
+    Router::add("GET", "/login", AuthController::class, "showLoginForm");
+    Router::add("POST", "/login", AuthController::class, "processLogin");
+    Router::add("GET", "/logout", AuthController::class, "logout");
+    Router::add("GET", "/dashboard", AuthController::class, "showDashboard");
+}
+
+function addUserRoutes()
+{
+    // user
+    Router::add("POST", "/dashboard", DashboardController::class, "showDenah");
+    Router::add("GET", "/pinjam", PinjamController::class, "showPinjamPage");
+    Router::add("GET", "/pesan", InboxController::class, "showInboxPage");
+    Router::add("GET", "/riwayat", HistoryController::class, "showHistoryPage");
+    Router::add("GET", "/konfirmasi-ruangan", RequestController::class, "showRequestPage");
+    Router::add("GET", "/profile", ProfileController::class, "showProfile");
+    Router::add("GET", "/detail", DetailRuanganController::class, "showDetailRuangan");
+    Router::add("POST", "/profile", AuthController::class, "changePass");
+}
+
+function addFormRoutes()
+{
+    // form
+    Router::add("GET", "/pinjam/form", MultiFormController::class, "showForm");
+    Router::add("POST", "/pinjam/form", MultiFormController::class, "handleForm");
+}
+
+function addAdminRoutes()
+{
+    // admin
+    Router::add("GET", "/inbox", AdminInboxController::class, "showInbox");
+    Router::add("GET", "/history", AdminHistoryController::class, "showHistory");
+    Router::add("GET", "/konfirmasiPinjam", AdminKonfirmasiController::class, "showKonfirmasiPinjamPage");
+}
