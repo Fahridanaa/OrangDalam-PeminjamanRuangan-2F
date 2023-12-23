b10f335b96f93499a02514f2c49b1997f5314ccd
Fahridanaa <fahridanaaa@gmail.com>
2023-12-23 09:38:01 +0700

    Update user profile and password change layout
    Incorporated the shared flash message into the user profile view. Adjusted design and dimensions of the profile and password change components for a more user-friendly layout. In the updated design, labels and input fields are more streamlined to ensure better alignment and consistency across the application.

--- app/Views/user/profile.php
+++ app/Views/user/profile.php
@@ -9,6 +9,7 @@ namespace OrangDalam\PeminjamanRuangan\Views\user;
     <?php include __DIR__ . '/../shared/head.php'; ?>
 </head>
 <body>
+<?php include __DIR__ . '/../shared/flashMessage.php' ?>
 <div id="user-profile" class="flex">
     <?php include 'sidebar.php'; ?>
     <div id="user-profile-content" class="h-screen w-screen py-20 px-8 flex flex-col gap-12 ml-32">
@@ -18,8 +19,8 @@ namespace OrangDalam\PeminjamanRuangan\Views\user;
                 <hr class="border border-black">
             </div>
             <div id="user-profile-card"
-                 class="flex justify-evenly items-center bg-neutral-color border-2 border[#C8C1C1] py-12 px-24 gap-24 rounded-3xl">
-                <div id="user-profile-pic" class="flex-[1] flex justify-center">
+                 class="flex justify-evenly items-center bg-neutral-color border-2 border[#C8C1C1] py-4 px-8 mx-auto gap-4 rounded-3xl min-w-[512px]">
+                <div id="user-profile-pic" class="flex-[1] flex justify-center min-w-[12rem]">
                     <img src="/img/user-picture.png" alt="user-profile-pic" class="rounded-full">
                 </div>
                 <div id="user-profile-data" class="flex-[3]">
@@ -46,29 +47,28 @@ namespace OrangDalam\PeminjamanRuangan\Views\user;
                 </div>
             </div>
         </div>
-        <div id="user-ganti-password" class="flex flex-col">
+        <div id="user-ganti-password">
             <div id="user-ganti-password-header" class="mb-6">
                 <h1 class="text-4xl font-semibold mb-6">Ganti Password</h1>
                 <hr class="border border-black">
             </div>
             <div id="user-ganti-password-card"
-                 class="self-center bg-neutral-color border-2 border[#C8C1C1] py-4 px-10 rounded-3xl w-3/4 mb-6">
-                <?php include __DIR__ . '/../shared/flashMessage.php' ?>
+                 class="self-center bg-neutral-color border-2 border[#C8C1C1] py-4 px-10 rounded-3xl mb-6">
                 <form method="POST" class="flex flex-col gap-8 my-4">
                     <div class="flex gap-2 justify-between items-center">
-                        <label for="password-sekarang">Password Sekarang</label>
+                        <label for="password-sekarang" class="min-w-[12rem]">Password Sekarang</label>
                         <input type="password" name="password-sekarang" id="password-sekarang"
-                               class="border border-black rounded-lg px-4 py-2 w-96" required>
+                               class="border border-black rounded-lg px-4 py-2 flex-auto max-w-[24rem]" required>
                     </div>
                     <div class="flex gap-2 justify-between items-center">
-                        <label for="password-baru">Password Baru</label>
+                        <label for="password-baru" class="min-w-[12rem]">Password Baru</label>
                         <input type="password" name="password-baru" id="password-baru"
-                               class="border border-black rounded-lg px-4 py-2 w-96 required">
+                               class="border border-black rounded-lg px-4 py-2 flex-auto max-w-[24rem]" required>
                     </div>
                     <div class="flex gap-2 justify-between items-center">
-                        <label for="konfirmasi-password-baru">Konfirmasi Password Baru</label>
+                        <label for="konfirmasi-password-baru" class="min-w-[12rem]">Konfirmasi Password Baru</label>
                         <input type="password" name="konfirmasi-password-baru" id="konfirmasi-password-baru"
-                               class="border border-black rounded-lg px-4 py-2 w-96" required>
+                               class="border border-black rounded-lg px-4 py-2 flex-auto max-w-[24rem]" required>
                     </div>
                     <button class="bg-third-color rounded-full px-10 py-2 text-neutral-color align-middle flex items-center self-end cursor-pointer hover:bg-primary-color"
                             type="submit">
