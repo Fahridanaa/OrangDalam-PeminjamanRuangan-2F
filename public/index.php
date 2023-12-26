882d8fce45f09a9e1d2cd609c560c370da574fb1
Muhammad Fakhruddin Arif <muhfakhruddin30@gmail.com>
2023-12-26 23:50:42 +0700

    Update Request

--- public/index.php
+++ public/index.php
@@ -54,6 +54,7 @@ function addUserRoutes()
     Router::add("GET", "/pesan", InboxController::class, "showInboxPage");
     Router::add("GET", "/riwayat", HistoryController::class, "showHistoryPage");
     Router::add("GET", "/konfirmasi-ruangan", RequestController::class, "showRequestPage");
+    Router::add("POST", "/konfirmasi-ruangan", RequestController::class, "updateStatus");
     Router::add("GET", "/profile", ProfileController::class, "showProfile");
     Router::add("GET", "/detail", DetailRuanganController::class, "showDetailRuangan");
     Router::add("POST", "/detail", DetailRuanganController::class, "getJadwal");
