cb499b31e743aa2e1d0096cf958571d174362698
Fahridanaa <fahridanaaa@gmail.com>
2023-12-27 21:35:51 +0700

    Detail matkul (#74)
    * Refine CSS, interactive elements and user profile data query
    
    Introduced a new CSS class for non-focused elements and adjusted UI interactive elements in sidebar.php and category.php for better user facilitation. Additionally, the user profile data query in AuthModel.php was amended to include 'ketua' details, further enhancing user profile information.
    
    * Add getProfileAdmin method and session implementation
    
    Added new getProfileAdmin method in AuthModel that fetches admin profile based on user id. Extended session logic in AuthController with admin's profile data, thus, enriching the overall user data information captured during a session.
    
    * Implement detail course view with dynamic data
    
    The changes primarily make room booking details dynamic, fetching data for the specific booked room with AJAX. This is achieved by adding a detail course method to the Peminjaman model, a handler method in the Request Controller and connecting them in the frontend with a click event listener. Relevant data is displayed in a modal box upon a button click providing an enhanced user experience.
    
    * Refactor detail course view and improve data fetch
    
    The code has been updated to make the room booking details more dynamic by using AJAX to fetch specific room details. The improvements included modifications in the Request Controller method, Peminjaman model, and the frontend interactivity. This resulted in a detailed course method that presents the data in a modal upon click, enhancing the overall user experience.

--- app/Views/user/konfirmasiRuangan.php
+++ app/Views/user/konfirmasiRuangan.php
@@ -67,8 +67,8 @@
                         . $btn .
                       '</div>
                             <div class="self-end flex gap-5">
-                                <button class="text-xl flex items-center gap-3" data-id=' . $item['id'] .'>
-                                    <span class="detail-matkul-button">Detail Informasi</span>
+                                <button class="detail-matkul-button text-xl flex items-center gap-3" data-id=' . $item['id'] .'>
+                                    <span>Detail Informasi</span>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                                     stroke="currentColor" class="w-6 h-6">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5"/>
@@ -106,11 +106,12 @@
             $dosenMatkul = document.getElementById('dosen-matkul');
             fetch('/konfirmasi-ruangan/data?id=' + id)
                 .then(response => response.json())
-                .then(data => {
+                .then(item => {
+                    const data = item[0];
                     document.getElementById('lantai-matkul').textContent = data.lantai;
                     document.getElementById('ruang-matkul').textContent = data.ruang;
                     document.getElementById('keterangan-matkul').textContent = data.keterangan;
-                    document.getElementById('tanggal-matkul').textContent = data.tanggalAcara;
+                    document.getElementById('tanggal-matkul').textContent = data.tanggal;
                     document.getElementById('matkul').textContent = data.matkul;
                     if($kelasMatkul != null) {
                         $kelasMatkul.textContent = data.kelas;
